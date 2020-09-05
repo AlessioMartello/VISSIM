f26e1b528eba735908ed7e54ea729e39c2c04f31
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-09-05 19:10:46 +0100

    initial commit

---
+++ VISSIM/methods/helpers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pathlib
+from datetime import datetime
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+# Function to load data from the VISSIM data files into a Pandas DataFrame
+def load_VISSIM_file(columns=None, use_cols=None, skiprows=0, nrows=None, index_col=False, sep="\s+", skipfooter=0):
+    raw_data = pd.read_csv(path, sep=sep, names=columns, header=None, engine="python", skiprows=skiprows,
+                           skipfooter=skipfooter, usecols=use_cols, index_col=index_col, nrows=nrows)
+    return raw_data
+
+# Function used to get the name of the data set from the files and use this to name the output file.
+def get_project_name():
+    df = load_VISSIM_file(None, None, 4, 1, False, sep="\s|:")
+    df = df.values.tolist()[0]
+    project_name = [element for element in df if element != "Comment" and type(element) == str]
+    project_name = " ".join(project_name)
+    return project_name
---
+++ VISSIM/methods/journey_times.py
@@ -0,0 +1,47 @@
+import helpers
+# Some values are hardcoded into the script (skipfooter and initial skiprows). These are not variable.
+# Any changing values have been considered and their calculation automated.
+# Such as the length of rows to skip
+
+results = pd.DataFrame()  # Initiate results DataFrame to append to on line 23
+use_cols = [col for col in range(1, 400, 2)] # Make a list of columns, to use when reading the DataFrame, every other column is useful.
+suffix = ".rsz" # Define filename suffix for Journey time analysis
+file_count=0 # Initialise file count for column names
+for path in pathlib.Path().iterdir():
+    if str(path).endswith(suffix):
+        excess_data = load_VISSIM_file(skiprows=8, skipfooter=5)  # Create a DataFrame including the excess data only
+        skip_length = len(
+            excess_data[excess_data[0].str.contains("No.")])  # Get the (length) number of rows containing excess data
+        relevant_data = load_VISSIM_file(sep=";", skiprows=(8 + skip_length), use_cols=use_cols
+                                         )  # load only the final data by skipping the length of the
+        # excess data. Not hardcoded because this is changeable
+        journey_times = relevant_data.drop([0, 1, 2, 3])  # Extract only the Journey Time row
+        results = results.append(journey_times)  # add the extracted data to a DataFrame
+        file_count +=1
+
+JT_route = relevant_data.drop([0, 1, 3, 4])  # Extract the Journey Time labels
+
+for col in use_cols:
+    try:
+        results[col] = pd.to_numeric(results[col])
+        JT_route[col] = pd.to_numeric(JT_route[col])
+    except KeyError:
+        continue
+
+avg = results.mean(axis=0)  # Average journey times
+results = results.append(avg, ignore_index=True)  # Add average to the end of the results DataFrame
+results = pd.concat([JT_route, results]).reset_index(drop=True)  # Add the respective Journey time labels
+results_transposed = results.transpose(copy=True)  # Transpose DataFrame for readability
+
+# Create list of columns, rename dataFrame
+files = ["Routes"]
+[files.append("Seed " + str(i+1)) for i in range(file_count)]
+files.append("Average")
+results_transposed.columns = files
+
+now = datetime.now()
+
+# Write results to an Excel file, timestamp ensures no overwriting
+writer = pd.ExcelWriter("JT results_" + now.strftime("%d-%m_%H.%M") + ".xlsx")
+results_transposed.to_excel(writer, "Journey time results", index=False)
+writer.save()
