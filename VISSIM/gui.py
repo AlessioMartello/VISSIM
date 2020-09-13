7518feb54e62993538accbcfddd17c5853c5c445
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-09-13 19:26:14 +0100

    all analyses functions linked to run function

--- VISSIM/gui.py
+++ VISSIM/gui.py
@@ -10,27 +10,31 @@ root.geometry("250x250")
 
 
 def open_directory_dialogue():
-    """Assigns the location of the selected directory to a variable"""
+    """Assigns the location of the selected directory to a global variable"""
     global data_directory
     data_directory = filedialog.askdirectory()
 
 
 def hit_and_run():
-    """ Runs the selected function in the checkButton widget"""
-    # Refactor me!!
-    #
-    if demand_dependency_state.get():
+    """ Runs the selected function(s) in the checkButton widget"""
+    # Refactor me to be more elegant!
+    if journey_time_state.get() and saturation_flow_state.get() and demand_dependency_state.get():
+        get_journey_times(data_directory) and get_saturation_flow(data_directory) and get_demand_dependencies(data_directory)
+    elif journey_time_state.get() and saturation_flow_state.get():
+        get_journey_times(data_directory) and get_saturation_flow(data_directory)
+    elif journey_time_state.get() and demand_dependency_state.get():
+        get_journey_times(data_directory) and get_demand_dependencies(data_directory)
+    elif saturation_flow_state.get() and demand_dependency_state.get():
+        get_saturation_flow(data_directory) and get_demand_dependencies(data_directory)
+    elif demand_dependency_state.get():
         get_demand_dependencies(data_directory)
-    # #
-    # elif saturation_flow_state.get():
-    #     get_saturation_flow(data)
-    #
-    # elif journey_time_state.get():
-    #     get_journey_times(data)
-    #
+    elif saturation_flow_state.get():
+        get_saturation_flow(data_directory)
+    elif journey_time_state.get():
+        get_journey_times(data_directory)
 
-intro_label = tk.Label(root, text="Select the analyses you would like to perform").pack()  # method adds the label
-# "greeting" to the window  # Label widget
+
+intro_label = tk.Label(root, text="Select the analyses you would like to perform").pack()
 
 demand_dependency_state, journey_time_state, saturation_flow_state = tk.BooleanVar(), tk.BooleanVar(), tk.BooleanVar()
 
@@ -38,10 +42,10 @@ demand_dependency_button = tk.Checkbutton(root, text="Demand dependency", variab
 journey_time_button = tk.Checkbutton(root, text="Journey time", variable=journey_time_state).pack()
 saturation_flow_button = tk.Checkbutton(root, text="Saturation flow", variable=saturation_flow_state).pack()
 
-select_data_button = tk.Button(root, text="Select data and run", state="active", command=open_directory_dialogue, padx=50,
+select_data_button = tk.Button(root, text="Select data folder", state="active", command=open_directory_dialogue, padx=50,
                                pady=15).pack()
 
 run = tk.Button(root, text="Run", state="active", command=hit_and_run, padx=50,
                 pady=15).pack()
 
-root.mainloop()  # Event loop
+root.mainloop()
